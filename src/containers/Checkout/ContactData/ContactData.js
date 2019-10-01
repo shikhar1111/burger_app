1251a0784f6542d4fe678f75e0c6faabba48d254
shikhar1111 <shikhar4frnds.agarwal11@gmail.com>
2019-10-01 14:58:30 +0530

    working with forms

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,9 +1,10 @@
-import React, {Component} from "react";
-import axios from "../../../axios-orders";
+import React, {Component} from "react"
 
+import axios from "../../../axios-orders"
 import Button from "../../../components/UI/Button/Button"
-import classes from "./ContactData.css";
-import Spinner from "../../../components/UI/Spinner/Spinner";
+import classes from "./ContactData.css"
+import Spinner from "../../../components/UI/Spinner/Spinner"
+import Input from "../../../components/UI/Input/Input"
 
 class ContactData extends Component {
     state = {
@@ -47,10 +48,10 @@ class ContactData extends Component {
 
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your name"/>
-                <input className={classes.Input} type="email" name="email" placeholder="Your email"/>
-                <input className={classes.Input} type="text" name="street" placeholder="Your Street"/>
-                <input className={classes.Input} type="text" name="postal" placeholder="Your Postal Code"/>
+                <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Your Street"/>
+                <Input inputtype="input" type="text" name="postal" placeholder="Your Postal Code"/>
                 <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
             </form>
         );
